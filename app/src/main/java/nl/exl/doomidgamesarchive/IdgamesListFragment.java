2ca81dea5f1348f6ff7aff6f39aafa400b741ded
Dennis Meuwissen <dennis@betawerk.nl>
2019-10-13 19:29:15 +0200

    Fix search button not working.

--- app/src/main/java/nl/exl/doomidgamesarchive/IdgamesListFragment.java
+++ app/src/main/java/nl/exl/doomidgamesarchive/IdgamesListFragment.java
@@ -134,7 +134,7 @@ public class IdgamesListFragment extends Fragment implements OnItemClickListener
         mSearchField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
                     mSearchQuery = v.getText().toString();
                     executeSearch();
                 }
